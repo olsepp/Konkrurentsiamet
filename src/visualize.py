41da57c3ed441fa12381b14b2cca37e9dff210fa
Oliver Sepp <olsepp@taltech.ee>
2025-05-18 14:54:35 +0300

    Added docstrings

--- src/visualize.py
+++ src/visualize.py
@@ -2,6 +2,9 @@ import matplotlib.pyplot as plt
 
 
 def plot_hourly_prices(df):
+
+    """Get JPG format image of a graph that displays electricity prices by hour."""
+
     plt.figure(figsize=(18, 6))
     plt.plot(df['Hour'], df['Price'], marker='o', linestyle='-')
     plt.title('Hourly Electricity Prices')
@@ -14,9 +17,15 @@ def plot_hourly_prices(df):
 
 
 def csv_metrics(df):
+
+    """Get CSV file of variety of different metrics based on prices on that day."""
+
     df.to_csv("metrics.csv", index=False)
 
 
 def csv_hourly_prices(df):
+
+    """Get CSV file of prices each hour."""
+
     df.to_csv("hourly_prices.csv", index=False)
 
