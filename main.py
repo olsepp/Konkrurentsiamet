a7d5d5684b3c3565304117ae841204d5af341fbf
Oliver Sepp <olsepp@taltech.ee>
2025-05-21 21:00:36 +0300

    Minor changes

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from src import visualize
 
 df = data_processing.get_dataframe()
 metrics_df = data_processing.get_summary_dataframe()
-# Use this to get a graph as jpg image
-img_result = visualize.plot_hourly_prices(df)
+# Use this to get a graph of hourly prices as jpg image
+visualize.plot_hourly_prices(df)
 
 # Use this to get csv file of hourly prices
-csv_result = visualize.csv_hourly_prices(df)
+visualize.csv_hourly_prices(df)
 
 # Use this to get additional metrics CSV file
-csv_metrics = visualize.csv_metrics(metrics_df)
+visualize.csv_metrics(metrics_df)
